eff004a402c446f89f1db8b34494cd57251c9dae
Dodoko <toni.ivankin.04@mail.ru>
2024-06-05 13:48:32 +0500

    step-2 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,9 +2,9 @@
 import { program } from 'commander';
 
 program
-  .version('0.1')  
-  .description('Compares two configuration files and shows a difference.')  
+  .version('0.1')
+  .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  
-  program.parse();
+  .option('-f, --format [type]', 'output format');
+
+program.parse();
