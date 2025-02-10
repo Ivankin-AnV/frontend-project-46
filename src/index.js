b5a5ed347a3be170d733927a78594f3af05b0691
toxai <toni.ivankin.04@mail.ru>
2025-02-10 14:36:05 +0500

    add index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import parse from './parsers.js';
+import { getFormat, readFile } from './utils.js';
+import buildTree from './buildTree.js';
+import formatter from './formatter/index.js';
+
+const genDiff = (filePath1, filePath2, format = 'stylish') => {
+  const file1 = readFile(filePath1);
+  const file2 = readFile(filePath2);
+
+  const obj1 = parse(file1, getFormat(filePath1));
+  const obj2 = parse(file2, getFormat(filePath2));
+
+  const tree = buildTree(obj1, obj2);
+  return formatter(tree, format);
+};
+export default genDiff;
